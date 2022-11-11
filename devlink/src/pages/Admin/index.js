b21d54b4edda6f87f60410df22e7169080328cef
Eduardo Silvaxl <eduardodasilvavicente471@gmail.com>
2022-11-10 21:20:29 -0300

    first commit

--- devlink/src/pages/Admin/index.js
+++ devlink/src/pages/Admin/index.js
@@ -1,7 +1,139 @@
+import { useState } from 'react'
+import './admin.css'
+import { Header } from '../../components/header'
+import { Logo } from '../../components/Logo'
+import { Input } from '../../components/input'
+
+import { MdAddLink } from 'react-icons/md'
+import { FiTrash2 } from 'react-icons/fi'
+
+import { db } from '../../services/fireBaseConnection'
+
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  doc,
+  deleteDoc
+} from 'firebase/firestore'
+
+import { toast } from 'react-toastify'
+
 export default function Admin(){
-    return(
-    <div>
-        <h1>Página ADMIN</h1>
+  const [nameInput, setNameInput] = useState("");  
+  const [urlInput, setUrlInput] = useState("");
+  const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1");
+  const [textColorInput, setTextColorInput] = useState("#121212");
+
+  
+  async function handleRegister(e){
+    e.preventDefault();
+  
+    if(nameInput === '' || urlInput === ''){
+      toast.warn("preencha todos os campos!")
+      return;
+    }
+
+    addDoc(collection(db, "links"), {
+      name: nameInput,
+      url: urlInput,
+      bg: backgroundColorInput,
+      color: textColorInput,
+      created: new Date(),
+    })
+    .then(() => {
+      setNameInput("")
+      setUrlInput("")
+      console.log("Link registrado com sucesso!")
+    })
+    .catch((error) => {
+      console.log("ERRO AO REGISTRAR" + error)
+      toast.error("Ops erro ao salvar o link")
+    })
+
+
+  }
+
+  
+  return(
+    <div className="admin-container">
+        <Header/>
+        
+        <Logo/>
+
+        <form className="form" onSubmit={handleRegister}>
+            
+            <label className="label">Nome do link</label>
+            <Input
+              placeholder="Nome do link..."
+              value={nameInput}
+              onChange={ (e) => setNameInput(e.target.value) }
+            />
+
+            <label className="label">Url do link</label>
+            <Input
+              type="url"
+              placeholder="Digite a url..."
+              value={urlInput}
+              onChange={ (e) => setUrlInput(e.target.value) }
+            />
+
+            <section className="container-colors">
+              <div>
+                <label className="label right">Fundo do link</label>
+                <input
+                  type="color"
+                  value={backgroundColorInput}
+                  onChange={ (e) => setBackgroundColorInput(e.target.value) }
+                
+                />
+              </div>
+
+              <div>
+                <label className="label right">Cor do link</label>
+                <input
+                  type="color"
+                  value={textColorInput}
+                  onChange={ (e) => setTextColorInput(e.target.value) }
+                />
+              </div>
+            </section>
+
+            { nameInput !== '' && (
+            <div className="preview">
+              <label className="label">Veja como está ficando 👇</label>
+              <article className="list" style={{ marginBottom: 8, marginTop: 8, backgroundColor: backgroundColorInput }}>
+                <p style={{ color: textColorInput }}>{nameInput}</p>
+              </article>
+            </div>
+            )}
+
+            <button className="btn-register" type="submit">
+              cadastrar <MdAddLink size={24} color="#FFF" />
+            </button>
+
+        
+        </form>
+
+
+        <h2 className="title">
+          Meus links
+        </h2>
+
+        <article 
+        className="list animate-pop"
+        style={{ backgroundColor: "#000", color: "#FFF" }}
+        >
+          <p>Grupo exclusivo no telegram</p>
+          <div>
+            <button className="btn-delete">
+              <FiTrash2 size={18} color="#FFF"/>
+            </button>
+          </div>
+        </article>
+
     </div>
   )
 }
--- devlink/src/routes/Private.js
+++ devlink/src/routes/Private.js
@@ -3,7 +3,8 @@ import { useState, useEffect } from 'react'
 import { auth } from '../services/fireBaseConnection'
 import { onAuthStateChanged } from 'firebase/auth'
 
-import { Navigate } from "react-router-dom"
+import { json, Navigate } from "react-router-dom"
+import { stringify } from '@firebase/util'
 
 export default function Private({ children }){
     const [loading, setLoading] = useState(true)
@@ -11,7 +12,30 @@ export default function Private({ children }){
 
     useEffect(() => {
         
-        console.log("COMPONENTE FOI CARREGADO")
+        async function checkLogin(){
+            const unsub = onAuthStateChanged(auth, (user) => {
+               
+
+                if(user){
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email
+                    };
+
+                    localStorage.setItem("@detailUser", JSON.stringify(userData))
+                    setLoading(false);
+                    setSigned(true)
+
+                }else{
+                    setLoading(false);
+                    setSigned(false)
+                }
+
+
+            })
+        }
+
+        checkLogin();
     
     }, [])
 
