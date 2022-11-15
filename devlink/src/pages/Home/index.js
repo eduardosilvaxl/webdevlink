10014a8f89ad9739e2d79e94ced51bd61577131e
eduardosilvaxl <eduardodasilvavicente471@gmail.com>
2022-11-14 21:29:09 -0300

    atualização do dia 14

--- devlink/src/pages/Home/index.js
+++ devlink/src/pages/Home/index.js
@@ -1,51 +1,118 @@
+import { useState, useEffect } from 'react'
+
 import './home.css'
 
 import { Social } from '../../components/Social'
 
 import { FaYoutube, FaTwitter, FaTwitch} from 'react-icons/fa'
 
+import { 
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc
+ } from 'firebase/firestore'
+
+ import { db } from '../../services/fireBaseConnection'
+
 export default function Home(){
-    return(
+
+  const [links, setLinks] = useState([]);
+  const [socialLinks, setSocialLinks] = useState({})
+    
+    useEffect(() => {
+      function loadLinks(){
+        const linksRef = collection(db, "links")
+        const queryRef = query(linksRef, orderBy("created", "asc"))
+
+        getDocs(queryRef)
+        .then((snapshot) => {
+          let lista = [];
+
+          snapshot.forEach((doc) => {
+            lista.push({
+              id:doc.id,
+              name: doc.data().name,
+              url: doc.data().url,
+              bg: doc.data().bg,
+              color: doc.data().color
+            })
+          })
+
+          setLinks(lista)
+
+        })
+
+
+      } 
+
+      loadLinks();
+
+    }, [])
+
+
+    useEffect(() => {
+
+      function loadSocialLinks(){
+        const docRef = doc(db, "social", "link")
+
+
+        getDoc(docRef)
+        .then((snapshot) => {
+
+          if(snapshot.data() !== undefined){
+            setSocialLinks({
+              youtube: snapshot.data().youtube,
+              twitter: snapshot.data().twitter,
+              twitch: snapshot.data().twitch,
+            })
+          }
+
+        })
+
+      }
+
+      loadSocialLinks();
+
+    }, [])
+  
+  return(
     <div className="home-container">
        <h1>Eduardo Silvaxl</h1>
        <span>Veja meus links 👇</span>
 
       <main className="links">
-      <section className="link-area">
-        <a href="#">
-          <p className="link-text">Canal no Youtube</p>
-        </a>
-       </section>
+      
+        { links.map((item) => (
+          <section key={item.id} className="link-area" style={{ backgroundColor: item.bg }} >
+            <a href={item.url} target="blank">
+              <p className="link-text" style={{color: item.color }} >
+                {item.name}
+              </p>
+            </a>
+          </section>
+        ))}
 
-       <section className="link-area">
-        <a href="#">
-          <p className="link-text">Canal na Twitch</p>
-        </a>
-       </section>
-
-       <section className="link-area">
-        <a href="#">
-          <p className="link-text">Grupo no Telegram</p>
-        </a>
-       </section>
-
-       <footer>
-          
-          <Social url="https://youtube.com/@eduardosilvaxl">
-            <FaYoutube size="35" color="#FFF" />
-          </Social>
-
-          <Social url="https://twitter.com/eduardo_silvaxl">
-            <FaTwitter size="35" color="#FFF" />
-          </Social>
-
-          <Social url="https://www.twitch.tv/urubeljoga">
-            <FaTwitch size="35" color="#FFF" />
-          </Social>
-
-       </footer>
-
+          {links.length !== 0 && Object.keys(socialLinks).length > 0 && (
+            <footer>
+                <Social url={socialLinks?.youtube}>
+                  <FaYoutube size="35" color="#FFF" />
+                </Social>
+      
+                <Social url={socialLinks?.twitter}>
+                  <FaTwitter size="35" color="#FFF" />
+                </Social>
+      
+                <Social url={socialLinks?.twitch}>
+                  <FaTwitch size="35" color="#FFF" />
+                </Social>
+            </footer>
+          )}
+  
       </main>
+    
     </div>
   )
 }
--- devlink/src/pages/Networks/index.js
+++ devlink/src/pages/Networks/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './networks.css'
 import { Input } from '../../components/input'
 import { MdAddLink } from 'react-icons/md'
@@ -12,14 +12,53 @@ import {
     getDoc,
 } from 'firebase/firestore'
 
-export default function Networks(){
-    const [facebook, setFacebook] = useState("");
-    const [instagram, setInstagram] = useState("");
-    const [youtube, setYoutube] = useState("");
+import { toast } from 'react-toastify'
 
-    async function handleSave(e){
+export default function Networks(){
+    const [youtube, setYoutube] = useState("");
+    const [twitter, setTwitter] = useState("");
+    const [twitch, setTwitch] = useState("");
+
+
+    useEffect(() => {
+      
+      function loadLinks(){
+        const docRef = doc(db, "social", "link")
+        getDoc(docRef)
+        .then((snapshot) => {
+          
+          if(snapshot.data() !== undefined) {
+            setYoutube(snapshot.data().youtube)
+            setTwitter(snapshot.data().twitter)
+            setTwitch(snapshot.data().twitch)
+          }
+
+        })
+
+      }
+
+      loadLinks();
+
+    }, [])
+
+
+    function handleSave(e){
       e.preventDefault();
       
+      setDoc(doc(db, "social", "link"), {
+        youtube: youtube,
+        twitter: twitter,
+        twitch: twitch
+      })
+      .then(() => {
+        console.log("Urls salvas com sucesso!")
+        toast.success("Salvo com sucesso!")
+      })
+      .catch((error) => {
+        console.log("ERRO AO SALVAR" + error)
+        toast.error("Erro ao salvar seus links!")
+      })
+
     }
 
 
@@ -30,25 +69,25 @@ export default function Networks(){
             <h1 className="title-social">Suas redes sociais</h1>
 
             <form className="form" onSubmit={handleSave}>
-              <label className="label">Link do facebook</label>
-              <Input
-                placeholder="Digite a url do facebook..."
-                value={facebook}
-                onChange={ (e) => setFacebook(e.target.value) }
-              />
-
-              <label className="label">Link do instagram</label>
-              <Input
-                placeholder="Digite a url do instagram..."
-                value={instagram}
-                onChange={ (e) => setInstagram(e.target.value) }
-              />  
-
               <label className="label">Link do youtube</label>
               <Input
                 placeholder="Digite a url do youtube..."
                 value={youtube}
                 onChange={ (e) => setYoutube(e.target.value) }
+              />
+
+              <label className="label">Link do twitter</label>
+              <Input
+                placeholder="Digite a url do twitter..."
+                value={twitter}
+                onChange={ (e) => setTwitter(e.target.value) }
+              />  
+
+              <label className="label">Link do twitch</label>
+              <Input
+                placeholder="Digite a url do twitch..."
+                value={twitch}
+                onChange={ (e) => setTwitch(e.target.value) }
               />  
 
               <button type="submit" className="btn-register">
